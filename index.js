542470cd5b75e4daeef53ef8adc04128cae978a4
曾志威 <51374393+DannyZeng2@users.noreply.github.com>
2019-06-18 21:49:58 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,14 +3,22 @@
 
 // 完成下面程序，将`name`变量中的字母全部转为为大写，输出：`'HELLO'`。
 var name = 'hello';
-// TODO write your code here...
+console.log(name.toUpperCase());
 
 
 // 完成下面程序，将sentence变量中的所有单词首字母变为大写，输出：'Good Afternoon, Mr Mike.'。
 var sentence = 'good afternoon, mr mike.';
-//TODO write your code here...
+var arr = sentence.split(" ");
+var newStr = "";
+
+arr.forEach((item,index,arr)=> { 
+    newStr += item[0].toUpperCase()+item.substring(1)+" ";  
+})
+console.log(newStr)
+
+
 
 
 // 完成下面程序，将money变量中的金额提取出来，输出：20。
 var money = '￥20';
-//TODO write your code here...
+console.log(money.substring(1));
